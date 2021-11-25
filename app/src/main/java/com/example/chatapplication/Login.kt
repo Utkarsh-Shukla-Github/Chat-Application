3224031c1d08b4906bef9eb9e9601607b7808ce2
Utkarsh Shukla <utkarshshukla707@gmail.com>
2021-11-25 15:25:18 +0530

    Authentication

--- app/src/main/java/com/example/chatapplication/Login.kt
+++ app/src/main/java/com/example/chatapplication/Login.kt
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+
+
 
 class Login : AppCompatActivity() {
 
@@ -12,10 +17,15 @@ class Login : AppCompatActivity() {
     private lateinit var edtPassword: EditText
     private lateinit var btnLogin: Button
     private lateinit var btnSignUp: Button
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        supportActionBar?.hide()
+
+        mAuth = FirebaseAuth.getInstance()
 
         edtEmail = findViewById(R.id.edt_email)
         edtPassword = findViewById(R.id.edt_password)
@@ -25,7 +35,26 @@ class Login : AppCompatActivity() {
         btnSignUp.setOnClickListener {
             val intent = Intent(this, SignUp::class.java)
             startActivity(intent)
-
         }
+
+        btnLogin.setOnClickListener{
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            login(email, password)
+        }
+    }
+
+    private fun login(email:String, password: String){
+        // logic for logging user
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this@Login, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@Login, "User does not exist", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
---
+++ app/src/main/java/com/example/chatapplication/User.kt
@@ -0,0 +1,5 @@
+package com.example.chatapplication
+
+class User {
+    
+}
